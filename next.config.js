a3b2f2a06eb6a190716967044188a90a90970688
Abhemanyu Sarin <abhemanyus@gmail.com>
2021-11-13 13:46:22 +0530

    clear

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const basePath = process.env.NODE_ENV === 'production' ? '' : '';
-
-module.exports = {
-  reactStrictMode: true,
-  basePath,
-  assetPrefix: `${basePath}/`,
-  images: {loader: 'custom'}
-};
