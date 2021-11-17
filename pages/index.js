915b7c6509a49de4f6ece6ecedbb3db664703ec6
Abhemanyu Sarin <abhemanyus@gmail.com>
2021-11-17 10:57:05 +0530

    descriptiong

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home () {
       </Head>
       <div>
         <h1 className="my-title">Homepage</h1>
-        <p className="my-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-        <p className="my-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <p className="my-desc">We are an organisation specializing in creating and deploying customized full-stack web-based solutions for everything from personal blogs to online businesses.</p>
+        <p className="my-desc">Our team has experts from every field required for creating the perfect product for your needs.</p>
         <Link href="/contact">
           <a className="block w-36 px-0 py-2 my-5 mx-auto bg-blue-600 rounded text-white text-center hover:bg-blue-500">Contact Us</a>
         </Link>
