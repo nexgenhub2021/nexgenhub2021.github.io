217f838cec5ddd1b87d8f58fb918b337f31c547a
Abhemanyu Sarin <abhemanyus@gmail.com>
2021-11-17 22:40:22 +0530

    ayayayay

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
-import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 export default function Home () {
   return (
-    <React.Fragment>
-      <Head>
-        <title>NexGenHub | Home</title>
-        <meta name="keywords" content="web"/>
-      </Head>
-      <div>
-        <h1 className="my-title">Homepage</h1>
-        <p className="my-desc">We are an organisation specializing in creating and deploying customized full-stack web-based solutions for everything from personal blogs to online businesses.</p>
-        <p className="my-desc">Our team has experts from every field required for creating the perfect product for your needs.</p>
-        <Link href="/contact">
-          <a className="block w-36 px-0 py-2 my-5 mx-auto bg-blue-600 rounded text-white text-center hover:bg-blue-500">Contact Us</a>
-        </Link>
+<section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+        <br className="hidden lg:inline-block"/>readymade gluten
+      </h1>
+      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"><Link href="/about">Button</Link></button>
+        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg"><Link href="/contact">Button</Link></button>
       </div>
-    </React.Fragment>
-  )
+    </div>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <Image className="object-cover object-center rounded" alt="hero" src="/images/dummy.png" height={600} width={720}/>
+    </div>
+  </div>
+</section>)
 }
